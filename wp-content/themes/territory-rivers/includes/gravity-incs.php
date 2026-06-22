71b05aa36b5fb9356b82c0d4d4ed6defe71eaf9b
Andrew <andrew@hyve.com.au>
2026-06-22 17:22:20 +1000

    theme update and test

--- wp-content/themes/territory-rivers/includes/gravity-incs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Gravity Forms remove TabIndex
-add_filter("gform_tabindex", fn() => false);
-
-// Gravity Forms remove Scroll to submit
-add_filter("gform_confirmation_anchor", fn() => false);
-
-?>
--- wp-content/themes/territory-rivers/partials/content-footer-signup.php
+++
@@ -1,6 +0,0 @@
-<div class="col-12 col-md-8 offset-md-2 sign-up-wrap">
-	<h2 ><?php the_field('join_title_footer', 'option') ?></h2>
-	<p><?php the_field('join_text_footer', 'option') ?><p>
-	<?php the_field('join_form_footer', 'option') ?>	
-	<?php the_field('collection_statement', 'option'); ?>
-</div>
